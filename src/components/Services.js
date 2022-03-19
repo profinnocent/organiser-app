aa74d679b48616c9dd566835ecf09838cbeed447
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-03-20 00:00:53 +0100

    Updated services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,24 +1,39 @@
 import "./Services.css";
-import heropix1 from "../assets/burger18.jpg";
-import heropix2 from "../assets/burger19.jpg";
-import heropix3 from "../assets/burger20.jpg";
-
+import heropix1 from "../assets/services2.jpg";
+import heropix2 from "../assets/burger18.jpg";
+import heropix3 from "../assets/services1.jpg";
 
 const Services = () => {
+ 
   return (
     <div className="services">
       <div className="hero">
-        <div className="pix">
-        <img src={heropix1} alt="" />
-        <img src={heropix2} alt="" />
-        <img src={heropix3} alt="" />
+        <h1>OUR SERVICES</h1>
+        <p>
+          We offer a wide range of indoor and outdoor services all for your
+          pleasure and convinience.
+        </p>
+
+        <div className="herocards">
+          <div className="herocard"> 
+          <img src={heropix1} alt="hero1" /> 
+          <h3>Home Delivery</h3>
+          <p>We deliver right to your doorsteps anywhere anytime and at very affordable rates.</p>
+          </div>
+
+          <div className="herocard"> 
+          <img src={heropix2} alt="hero2" /> 
+          <h3>Special Occassion Catering</h3>
+          <p>We will go extra miles to make those special moments memorable.</p>
+          </div>
+
+          <div className="herocard"> 
+          <img src={heropix3} alt="By Rachel Claire from Pexels" /> 
+          <h3>Outdoor Catering Services</h3>
+          <p>Book us to cover your catering needs so you can focus on enjoying your vacations.</p>
+          </div>
 
         </div>
-        {/* <div className="herowords"> */}
-          <h1>OUR SERVICES</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <button>SHOP</button>
-        {/* </div> */}
       </div>
     </div>
   );
