f83e0ef96a3e3b3a71d4787ae9560f8023f2d106
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-03-25 22:02:45 +0100

    worked on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,63 @@
-import './Contact.css'
-import pixe from '../assets/bking-bg1.png'
+import "./Contact.css";
+import pixe from "../assets/bking-bg1.png";
 
 const Contact = () => {
   return (
-    <div className='contact'> 
-    <h1>CONTACT</h1>
-    <p>We are super excited to hear from you!</p>
-    <div className="formset">
+    <div className="contact">
+            <div className="formset">
 
-      <form >
-        <div className="form-group">
-          <label htmlFor="name">
-            First Name
-          </label>
-          <input name="fname" id="fname" type="text" placeholder="Please enter your full names"/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="lname">
-            Last Name
-          </label>
-          <input name="lname" id="lname" type="text" placeholder="Please enter your full names"/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">
-            Email
-          </label>
-          <input name="email" id="email" type="text" placeholder="Please enter your full names"/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">
-            Message
-          </label>
-          <textarea name="message" id="message" type="text" placeholder="Please enter your message here" rows="10"/>
-        </div>
-          <input value="submit" id="submit" type="submit" placeholder="Please enter your full names"/>
+      <h1>CONTACT</h1>
+      <p>We are super excited to hear from you!</p>
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">First Name</label>
+            <input
+              name="fname"
+              id="fname"
+              type="text"
+              placeholder="Please enter your first name here"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lname">Last Name</label>
+            <input
+              name="lname"
+              id="lname"
+              type="text"
+              placeholder="Please enter your last name here"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              name="email"
+              id="email"
+              type="text"
+              placeholder="Please enter your email here"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              type="text"
+              placeholder="Please enter your message here"
+              rows="10"
+            />
+
+<button id="submit" onClick={e => e.preventDefault()}>Send <i class="fa-solid fa-paper-plane"> </i></button>
+
+          </div>
+        </form>
         
-      </form>
+      </div>
       <div className="formpix">
           <img src={pixe} alt="pixe" />
         </div>
-      </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
